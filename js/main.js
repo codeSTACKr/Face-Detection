1ba0792d770bfe51b8548d914915cc59ad5206da
codeSTACKr <52665907+codeSTACKr@users.noreply.github.com>
2020-02-12 14:39:48 -0600

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+const video = document.getElementById("video");
+let predictedAges = [];
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.ageGenderNet.loadFromUri("/models")
+]).then(startVideo);
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => (video.srcObject = stream),
+    err => console.error(err)
+  );
+}
+
+video.addEventListener("playing", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withAgeAndGender();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+
+    const age = resizedDetections[0].age;
+    const interpolatedAge = interpolateAgePredictions(age);
+    const bottomRight = {
+      x: resizedDetections[0].detection.box.bottomRight.x - 50,
+      y: resizedDetections[0].detection.box.bottomRight.y
+    };
+
+    new faceapi.draw.DrawTextField(
+      [`${faceapi.utils.round(interpolatedAge, 0)} years`],
+      bottomRight
+    ).draw(canvas);
+  }, 100);
+});
+
+function interpolateAgePredictions(age) {
+  predictedAges = [age].concat(predictedAges).slice(0, 30);
+  const avgPredictedAge =
+    predictedAges.reduce((total, a) => total + a) / predictedAges.length;
+  return avgPredictedAge;
+}
